9cf6b292a9bcb19d51e5af185af3b0a51a49e80f
Rolexo <105015504+Rolexo@users.noreply.github.com>
2022-06-08 08:41:59 +0300

    Add files via upload
    program asks user for age and tells them what they can drive

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER YOUR AGE: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 4 && age < 18)
+            {
+                Console.WriteLine("yoo can ride a bike");
+
+            }
+            else if (age >= 30 && age < 80)
+            {
+                Console.WriteLine("you can drive a lorry");
+
+            }
+            else
+            {
+                Console.WriteLine("just enjoy your life");
+
+            } 
+        }
+    }
+}
